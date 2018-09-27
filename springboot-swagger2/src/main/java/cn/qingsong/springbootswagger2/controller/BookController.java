c0e12d695b52b1782b1a772743db0339dd05f658
hanqingsong <hanqingsonga@163.com>
2018-09-27 14:19:03 +0800

    spingboot swagger 接口描述

--- springboot-swagger2/src/main/java/cn/qingsong/springbootswagger2/controller/BookController.java
+++ springboot-swagger2/src/main/java/cn/qingsong/springbootswagger2/controller/BookController.java
@@ -4,6 +4,11 @@ import cn.qingsong.springbootswagger2.model.Book;
 import cn.qingsong.springbootswagger2.model.User;
 import cn.qingsong.springbootswagger2.result.WebResponse;
 import com.google.common.collect.Maps;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +28,31 @@ import java.util.Map;
 @RequestMapping("/book")
 public class BookController {
 
+
     @ResponseBody
     @GetMapping("/info")
     public WebResponse getInfo(String lid) {
         Map<String, Object> map = Maps.newHashMap();
         return new WebResponse();
     }
-
+    /**
+     *  @ApiOperation api接口描述 value方法名描述 notes接口信息描述 tags标签名分组
+     *  @ApiImplicitParams api参数说明，描述，类型，是否是必须
+     *  @ApiResponses 返回值说明
+     *
+     *  @ApiModel和@ApiModelProperty
+     * @param book
+     * @return
+     */
+    @ApiOperation(value = "添加教材value", notes = "添加教材notes", /*tags = "标签book",*/httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "名称", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "price", value = "价格", required = true, dataType = "Double"),
+            @ApiImplicitParam(name = "repertory", value = "库存", required = true, dataType = "Integer")
+    })
+    @ApiResponses({
+            @ApiResponse(code=200,message="成功",response=WebResponse.class),
+    })
     @ResponseBody
     @PostMapping("/save")
     public WebResponse saveInfo(@RequestBody Book book) {
--- springboot-swagger2/src/main/java/cn/qingsong/springbootswagger2/model/Book.java
+++ springboot-swagger2/src/main/java/cn/qingsong/springbootswagger2/model/Book.java
@@ -1,14 +1,21 @@
 package cn.qingsong.springbootswagger2.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by hanqingsong on 18/9/25.
  *
  * @author hanqingsong
  * @date 18/9/25
  */
+@ApiModel(value="Book对象",description="Book对象说明")
 public class Book {
+    @ApiModelProperty(name = "name",value = "书名",required = true,example = "测试书名")
     private String name;
+    @ApiModelProperty(name = "price",value = "价格",required = true,example = "2.1")
     private double price;
+    @ApiModelProperty(name = "repertory",value = "价格",required = true,example = "1")
     private Integer repertory;
 
     public String getName() {
