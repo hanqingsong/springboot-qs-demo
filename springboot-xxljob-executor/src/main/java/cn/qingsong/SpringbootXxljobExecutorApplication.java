243faf0faf094fad04433c46e6b555e9d12f118f
hanqingsong <842071912@qq.com>
2019-07-20 18:09:44 +0800

    xxljob

---
+++ springboot-xxljob-executor/src/main/java/cn/qingsong/SpringbootXxljobExecutorApplication.java
@@ -0,0 +1,13 @@
+package cn.qingsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXxljobExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXxljobExecutorApplication.class, args);
+	}
+
+}
---
+++ springboot-xxljob-executor/src/test/java/cn/qingsong/SpringbootXxljobExecutorApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.qingsong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootXxljobExecutorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
